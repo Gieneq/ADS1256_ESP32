8ef86aebc85ed16d4bd0e0000ec39a494e043d17
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 02:02:13 +0100

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,54 @@
-#include <Arduino.h>
+// Arudino Sample Code to use ADS1256 library
+// http://www.ti.com/lit/ds/symlink/ads1256.pdf
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "ADS1256.h"
+#include <SPI.h>
+
+float clockMHZ = 7.68; // crystal frequency used on ADS1256
+float vRef = 2.47; // voltage reference
+
+// Construct and init ADS1256 object
+ADS1256 adc(clockMHZ,vRef,false); // RESETPIN is permanently tied to 3.3v
+
+float sensor1;
+SPIClass* spiobject;
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  Serial.println("Starting ADC");
+
+  // start the ADS1256 with data rate of 15 SPS and gain x1
+  adc.begin(ADS1256_DRATE_30000SPS,ADS1256_GAIN_1,false); 
+  spiobject = adc.getSPI();
+ 
+  // Set MUX Register to AINO so it start doing the ADC conversion
+  Serial.println("Channel set to Single end ch0");
+  adc.setChannel(0);
+    digitalWrite(pinCS, LOW);  //CS LOW
+    spiobject->transfer(ADS1256_CMD_RDATAC); //contin
+    digitalWrite(pinCS, HIGH);
+  delayMicroseconds(7);  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    digitalWrite(pinCS, LOW);  //CS LOW
+    while (digitalRead(pinDRDY)); //w8 til DRDY goes LOW
+
+    uint8_t buff[] = {0,0,0}; 
+    unsigned long value;
+
+    spiobject->transfer(buff, 3);
+    value = ((long)buff[2] << 16) + ((long)buff[1] << 8) + ((long)buff[0]);
+
+    float voltage = 2*2.47 * value / (1*0x7FFFFF);
+    // Serial.print("V "); Serial.print(voltage, DEC);
+    // Serial.print(", "); Serial.print(value, DEC);
+    // Serial.println(".");
+
+    digitalWrite(pinCS, HIGH);
+    delayMicroseconds(7);  
 }
